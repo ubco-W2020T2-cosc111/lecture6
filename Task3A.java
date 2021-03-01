92ae68c509f010f573f106ebb43e96ff771a3447
AashishChilwal <78070029+AashishChilwal@users.noreply.github.com>
2021-03-01 16:01:41 -0700

    Finished Task 3A

--- Task3A.java
+++ Task3A.java
@@ -1,1 +1,11 @@
+public class Task3A {
+    public static void main(String []args){
+        if (x>10)
+            score = 3*scale;
+        
+        else 
+           score =4*scale;
+
+    }
+}
 
